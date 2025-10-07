2b0c9212db9879f4e1dd6d80dfdab4f748cd6e56
LucaTvl <ltrincavell@frro.utn.edu.ar>
2025-10-07 00:02:33 -0300

    fix: fixed app.ts

--- src/app.ts
+++ src/app.ts
@@ -9,15 +9,11 @@ import { errorHandler } from './shared/middlewares/errorHandler.js'
 const app = express()
 const PORT = process.env.PORT || 4000
 
-/*
 app.use(
   cors({
     origin: process.env.FRONTEND_URL || 'http://localhost:5173',
   }),
 )
-*/
-
-app.use(cors())
 
 app.use(express.json())
 app.use(ROUTES)
