320044e87c8e164d87f6b2f6afba9fd431c03e5a
diegolezcanodev <diegolezcano209@gmail.com>
2025-10-10 01:04:08 -0300

    Update vehiculo.routes.ts

--- src/models/Vehiculo/vehiculo.routes.ts
+++ src/models/Vehiculo/vehiculo.routes.ts
@@ -4,7 +4,6 @@ import { validate } from '../../shared/middlewares/validateSchemas.js'
 import {
   createVehiculoSchema,
   updateVehiculoSchema,
-  patenteParamsSchema,
 } from 'sigma-la-schemas'
 const vehiculoController = new VehiculoController()
 const vehiculoRouter = Router()
@@ -23,7 +22,6 @@ vehiculoRouter.post(
 
 vehiculoRouter.get(
   '/:patente',
-  validate({ params: patenteParamsSchema }),
   (req, res) => {
     vehiculoController.getOne(req, res)
   },
@@ -32,7 +30,6 @@ vehiculoRouter.get(
 vehiculoRouter.put(
   '/:patente',
   validate({
-    params: patenteParamsSchema,
     body: updateVehiculoSchema,
   }),
   (req, res) => {
@@ -42,7 +39,6 @@ vehiculoRouter.put(
 
 vehiculoRouter.delete(
   '/:patente',
-  validate({ params: patenteParamsSchema }),
   (req, res) => {
     vehiculoController.remove(req, res)
   },
