e4c6827b7ff2ff309a067c3bbac2e7a5cb801e7e
Luhm4nn <emyluhmann@gmail.com>
2025-10-18 18:19:48 -0300

    refactor: update login method to include refresh token

--- src/app.ts
+++ src/app.ts
@@ -9,15 +9,12 @@ import { errorHandler } from './shared/middlewares/errorHandler.js'
 const app = express()
 const PORT = process.env.PORT || 4000
 
-/*
 app.use(
   cors({
     origin: process.env.FRONTEND_URL || 'http://localhost:5173',
+    credentials: true,
   }),
 )
-*/
-
-app.use(cors())
 
 app.use(express.json())
 app.use(ROUTES)
